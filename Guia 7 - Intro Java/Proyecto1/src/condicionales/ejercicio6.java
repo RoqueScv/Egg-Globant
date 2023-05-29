8ba3b77288f2ad1b7fa88e897beb42ede202200e
Roque Scovenna <roque.scovenna@gmail.com>
2023-05-29 20:12:50 -0300

    asd

---
+++ Guia 7 - Intro Java/Proyecto1/src/condicionales/ejercicio6.java
@@ -0,0 +1,17 @@
+package condicionales;
+
+import java.util.Scanner;
+
+public class ejercicio6 {
+        public static void main(String[] args) {
+            Scanner leer =new Scanner(System.in).useDelimiter("\n");
+            System.out.println("Ingrese un numero");
+            int num=leer.nextInt();
+            if (0==num%2) {
+                System.out.println("El numero es par");
+            } else {
+                System.out.println("El numero es impar");
+            }
+                    
+        }
+}
---
+++ Guia 7 - Intro Java/Proyecto1/src/intro/ejercicio4.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+F = 32 + (9 * C / 5).
+ */
+package intro;
+
+import java.util.Scanner;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+    float gradosC;
+    System.out.println("Ingrese la temperatura en grados centigrados:");
+    Scanner read = new Scanner(System.in).useDelimiter("\n");
+    gradosC=read.nextFloat();
+    System.out.println("La temperatura en grados Farenheit es: " + (32 + (9 * gradosC / 5)));
+    }
+}
